5dd20a7b6ee91b52f943f527e27d61e95c012b3f
Deven Shah <devenshah1974@gmail.com>
2024-05-24 17:24:11 +0100

    CSP-1384 Configured NServiceBus for message handlers

--- src/SFA.DAS.PR.Jobs/Functions/PingFunction.cs
+++ src/SFA.DAS.PR.Jobs/Functions/PingFunction.cs
@@ -6,13 +6,22 @@ using SFA.DAS.PR.Data.Repositories;
 
 namespace SFA.DAS.PR.Jobs.Functions;
 
-public class PingFunction(ILogger<PingFunction> _logger, IProvidersRepository _providersRepository)
+public class PingFunction(ILogger<PingFunction> _logger, IProvidersRepository _providersRepository, IFunctionEndpoint _functionEndpoint)
 {
     [Function("ping")]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req, CancellationToken cancellationToken)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req, FunctionContext executionContext, CancellationToken cancellationToken)
     {
+        await _functionEndpoint.Publish(new HelloWorldEvent(executionContext.FunctionId), executionContext);
+
         var providersCount = await _providersRepository.GetCount(cancellationToken);
+
         if (providersCount == 0) _logger.LogWarning("Providers cache is empty");
-        return new OkObjectResult($"You have successfully invoked Ping function in Provider Relationships Jobs! at {DateTime.UtcNow}. The current count of providers is {providersCount}");
+
+        var str = $"You have successfully invoked Ping function in Provider Relationships Jobs! at {DateTime.UtcNow}. The current count of providers is {providersCount}";
+        _logger.LogInformation(str);
+
+        return new OkObjectResult(new { executionContext.FunctionId, providersCount, DateTime.UtcNow });
     }
 }
+
+public record HelloWorldEvent(string FunctionId);
--- src/SFA.DAS.PR.Jobs/Program.cs
+++ src/SFA.DAS.PR.Jobs/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -6,10 +7,26 @@ using SFA.DAS.PR.Jobs.Extensions;
 
 [assembly: NServiceBusTriggerFunction("SFA.DAS.PR.Jobs")]
 
+const string ErrorEndpointName = $"SFA.DAS.PR.Jobs-error";
+
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .UseNServiceBus()
     .ConfigureAppConfiguration(builder => builder.AddConfiguration())
+    .UseNServiceBus((configuration, endpointConfiguration) =>
+    {
+        endpointConfiguration.AdvancedConfiguration.EnableInstallers();
+
+        endpointConfiguration.AdvancedConfiguration.Conventions()
+            .DefiningCommandsAs(t => Regex.IsMatch(t.Name, "Command(V\\d+)?$"))
+            .DefiningEventsAs(t => Regex.IsMatch(t.Name, "Event(V\\d+)?$"));
+
+        endpointConfiguration.AdvancedConfiguration.SendFailedMessagesTo(ErrorEndpointName);
+
+        var persistence = endpointConfiguration.AdvancedConfiguration.UsePersistence<AzureTablePersistence>();
+        persistence.ConnectionString(configuration["AzureWebJobsStorage"]);
+
+        endpointConfiguration.AdvancedConfiguration.License(configuration["NServiceBusConfiguration:NServiceBusLicense"]);
+    })
     .ConfigureServices((context, services) =>
     {
         services
