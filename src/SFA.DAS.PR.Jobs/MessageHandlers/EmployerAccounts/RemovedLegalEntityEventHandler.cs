646a855e67004db85320c6672a22e862a7427f61
shaun-downey-education <Shaun.Downey@education.gov.uk>
2024-07-01 11:12:10 +0100

    CSP-1483: RemovedLegalEntityEventHandler initial commit.

--- src/SFA.DAS.PR.Jobs/MessageHandlers/EmployerAccounts/RemovedLegalEntityEventHandler.cs
+++ src/SFA.DAS.PR.Jobs/MessageHandlers/EmployerAccounts/RemovedLegalEntityEventHandler.cs
@@ -1,32 +1,98 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.EmployerAccounts.Messages.Events;
 using SFA.DAS.PR.Data;
+using SFA.DAS.PR.Data.Entities;
+using SFA.DAS.PR.Jobs.Models;
 
 namespace SFA.DAS.PR.Jobs.MessageHandlers.EmployerAccounts;
 
-public class RemovedLegalEntityEventHandler(IProviderRelationshipsDataContext _providerRelationshipsDataContext, ILogger<RemovedLegalEntityEventHandler> _logger) : IHandleMessages<CreatedAccountEvent>
+//public class SendDeletedPermissionsNotificationCommand : IRequest
+//{
+//    public long Ukprn { get; set; }
+//    public long AccountLegalEntityId { get; set; }
+
+//    public SendDeletedPermissionsNotificationCommand
+//    (
+//        long ukprn,
+//        long accountLegalEntityId
+//    )
+//    {
+//        Ukprn = ukprn;
+//        AccountLegalEntityId = accountLegalEntityId;
+//    }
+//}
+
+public record PermissionAuditDetails(long accountId, long accountLegalEntityId, long Ukprn, IEnumerable<Operation> operations);
+
+public class RemovedLegalEntityEventHandler(IProviderRelationshipsDataContext _providerRelationshipsDataContext, ILogger<RemovedLegalEntityEventHandler> _logger) : IHandleMessages<RemovedLegalEntityEvent>
 {
-    public const string RemovedLegalEntityEventFailureReason = "Account already exists";
-    public async Task Handle(CreatedAccountEvent message, IMessageHandlerContext context)
+    public const string RemovedLegalEntityEventFailureReason = "Account legal entity does not exists";
+
+    public async Task Handle(RemovedLegalEntityEvent message, IMessageHandlerContext context)
     {
-        // new EventHandlerJobInfo<CreatedAccountEvent>(context.MessageId, message, false, AccountAlreadyExistsFailureReason)
+        AccountLegalEntity? accountLegalEntity = await _providerRelationshipsDataContext.AccountLegalEntities
+            .Include(a => a.AccountProviderLegalEntities)
+                .ThenInclude(b => b.Permissions)
+            .Include(a => a.AccountProviderLegalEntities)
+                .ThenInclude(b => b.AccountProvider)
+        .FirstOrDefaultAsync(a => a.Id == message.AccountLegalEntityId, context.CancellationToken);
 
-        // new(nameof(AddedLegalEntityEventHandler),
-        // new EventHandlerJobInfo<CreatedAccountEvent>(context.MessageId, message, true, null))
+        if (accountLegalEntity is null)
+        {
+            await _providerRelationshipsDataContext.JobAudits.AddAsync(
+                new(
+                    nameof(RemovedLegalEntityEventHandler),
+                    new EventHandlerJobInfo<RemovedLegalEntityEvent>(context.MessageId, message, false, RemovedLegalEntityEventFailureReason)
+                ),
+                context.CancellationToken
+            );
 
-        // Validation: Account legal entity should exist, otherwise just create audit record with failure reason. 
+            await _providerRelationshipsDataContext.SaveChangesAsync(context.CancellationToken);
+        }
+        else
+        {
+            // For all the permissions that are being deleted, we need to send out notifications. In the current implementation this is
+            // done via raising DeletedPermissionsEventV2 event, however we are not going to use events for internal changes,
+            // so the logic that is part of DeletedPermissionsEventV2Handler should be included in here.
 
-        // We need to do the following :
+            IEnumerable<Permission> permissions = accountLegalEntity.AccountProviderLegalEntities.SelectMany(a => a.Permissions);
 
-        // Delete all the AccountProviderLegalEntities records associated with this account legal entity
+            // Create audit record for each permission deleted.
+            // Audit info should capture: AccountId, AccountLegalEntityId, Ukprn, Existing Permissions:
 
-        // Set Deleted on AccountLegalEntity
-        // Delete all the permissions record associated with this account legal entity
+            IEnumerable<PermissionAuditDetails> auditDetails = accountLegalEntity.AccountProviderLegalEntities.Select(a =>
+                new PermissionAuditDetails(
+                    message.AccountId,
+                    message.AccountLegalEntityId,
+                    a.AccountProvider.ProviderUkprn,
+                    a.Permissions.SelectMany(a => a.Operation) //  operations
+                )
+            ); // Single
 
-        // For all the permissions that are being deleted, we need to send out notifications.In the current implementation this is done via raising DeletedPermissionsEventV2 event, however we are not going to use events for internal changes, so the logic that is part of DeletedPermissionsEventV2Handler should be included in here.
+            // Delete all the permissions record associated with this account legal entity.
 
-        // Create audit record for each permission deleted.Audit info should capture, AccountId, AccountLegalEntityId, Ukprn, Existing Permissions
+            _providerRelationshipsDataContext.Permissions.RemoveRange(permissions);
 
-        await _providerRelationshipsDataContext.SaveChangesAsync(context.CancellationToken);
+            // Delete all the AccountProviderLegalEntities records associated with this account legal entity.
+            
+            _providerRelationshipsDataContext.AccountProviderLegalEntities.RemoveRange(
+                accountLegalEntity.AccountProviderLegalEntities
+            );
+            
+            accountLegalEntity.Deleted = message.Created;
+            
+            // Create audit record for each permission deleted.
+            //jobAudit = new(
+            //    nameof(RemovedLegalEntityEventHandler),
+            //    new EventHandlerJobInfo<RemovedLegalEntityEvent>(context.MessageId, message, true, null)
+            //);
+            //await _providerRelationshipsDataContext.JobAudits.AddRangeAsync(//JobAudits, context.CancellationToken);
+
+            await _providerRelationshipsDataContext.SaveChangesAsync(context.CancellationToken);
+        }
+
+        // Set Deleted on AccountLegalEntityt
+
     }
 }
