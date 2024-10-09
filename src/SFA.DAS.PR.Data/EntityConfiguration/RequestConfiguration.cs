6d4d036532a8d20f8214911b6a3e15e04297b868
shaun-downey-education <shaun.downey@education.gov.uk>
2024-10-09 17:01:16 +0100

    CSP-1617: Add in json deserializer for RequestStatus and RequestType.

--- src/SFA.DAS.PR.Data/EntityConfiguration/RequestConfiguration.cs
+++ src/SFA.DAS.PR.Data/EntityConfiguration/RequestConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SFA.DAS.PR.Data.Entities;
 using System.Diagnostics.CodeAnalysis;
 
@@ -11,5 +12,11 @@ public class RequestConfiguration : IEntityTypeConfiguration<Request>
     public void Configure(EntityTypeBuilder<Request> builder)
     {
         builder.HasKey(p => p.Id);
+
+        builder.Property(e => e.Status)
+               .HasConversion(new EnumToStringConverter<RequestStatus>());
+
+        builder.Property(e => e.RequestType)
+               .HasConversion(new EnumToStringConverter<RequestType>());
     }
 }
