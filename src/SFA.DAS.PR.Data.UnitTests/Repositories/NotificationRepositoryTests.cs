9e8972dab2edade064403c636338cffbdee437bd
shaun-downey-education <shaun.downey@education.gov.uk>
2024-10-25 16:59:49 +0100

    CSP-1662: Clean up notification function - unit test coverage.

--- src/SFA.DAS.PR.Data.UnitTests/Repositories/NotificationRepositoryTests.cs
+++ src/SFA.DAS.PR.Data.UnitTests/Repositories/NotificationRepositoryTests.cs
@@ -33,4 +33,82 @@ public class NotificationRepositoryTests
         var result = await sut.GetPendingNotifications(100, CancellationToken.None);
         Assert.That(result, Is.Empty);
     }
+
+    [Test]
+    public async Task NotificationsRepository_GetExpiredNotifications_Returns_Success()
+    {
+        Notification notification = NotificationData.Create(Guid.NewGuid(), NotificationType.Provider, 10000001, 1, "TemplateName");
+        notification.CreatedDate = DateTime.UtcNow.AddDays(-366);
+
+        using var context = DbContextHelper
+            .CreateInMemoryDbContext()
+            .AddNotification(notification)
+            .PersistChanges();
+
+        NotificationRepository sut = new NotificationRepository(context);
+        var result = await sut.GetExpiredNotifications(365, CancellationToken.None);
+        Assert.That(result, Has.Count.EqualTo(1));
+    }
+
+    [Test]
+    public async Task NotificationsRepository_GetExpiredNotifications_Returns_Empty()
+    {
+        using var context = DbContextHelper.CreateInMemoryDbContext();
+
+        NotificationRepository sut = new NotificationRepository(context);
+        var result = await sut.GetExpiredNotifications(365, CancellationToken.None);
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public async Task NotificationsRepository_GetExpiredNotifications_BoundaryLineExpired_Returns_Collection()
+    {
+        DateTime pastDate = DateTime.UtcNow.AddDays(-366);
+        DateTime boundaryLine = new DateTime(pastDate.Year, pastDate.Month, pastDate.Day, 00, 00, 01);
+
+        Notification notification = NotificationData.Create(
+            Guid.NewGuid(), 
+            NotificationType.Provider, 
+            10000001, 
+            1, 
+            "TemplateName"
+        );
+
+        notification.CreatedDate = boundaryLine;
+
+        using var context = DbContextHelper
+            .CreateInMemoryDbContext()
+            .AddNotification(notification)
+            .PersistChanges();
+
+        NotificationRepository sut = new NotificationRepository(context);
+        var result = await sut.GetExpiredNotifications(365, CancellationToken.None);
+        Assert.That(result.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task NotificationsRepository_GetExpiredNotifications_BoundaryLineExpired_Returns_Empty()
+    {
+        DateTime pastDate = DateTime.UtcNow.AddDays(-365);
+        DateTime boundaryLine = new DateTime(pastDate.Year, pastDate.Month, pastDate.Day + 1, 00, 00, 01);
+
+        Notification notification = NotificationData.Create(
+            Guid.NewGuid(),
+            NotificationType.Provider,
+            10000001,
+            1,
+            "TemplateName"
+        );
+
+        notification.CreatedDate = boundaryLine;
+
+        using var context = DbContextHelper
+            .CreateInMemoryDbContext()
+            .AddNotification(notification)
+            .PersistChanges();
+
+        NotificationRepository sut = new NotificationRepository(context);
+        var result = await sut.GetExpiredNotifications(365, CancellationToken.None);
+        Assert.That(result.Count, Is.EqualTo(0));
+    }
 }
