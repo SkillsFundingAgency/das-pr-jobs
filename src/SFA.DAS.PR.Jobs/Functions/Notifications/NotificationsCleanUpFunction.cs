1f20a9005ff91ed60b0c071500c0d7c1c935b03c
shaun-downey-education <shaun.downey@education.gov.uk>
2024-10-25 16:29:39 +0100

    CSP-1662: Notification clean up job. - template json - NotificationsCleanUpFunctionSchedule setting added.

---
+++ src/SFA.DAS.PR.Jobs/Functions/Notifications/NotificationsCleanUpFunction.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Options;
+using SFA.DAS.PR.Data;
+using SFA.DAS.PR.Data.Repositories;
+using SFA.DAS.PR.Jobs.Configuration;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SFA.DAS.PR.Jobs.Functions.Notifications;
+
+public sealed class NotificationsCleanUpFunction
+{
+    private readonly INotificationRepository _notificationRepository;
+    private readonly IProviderRelationshipsDataContext _providerRelationshipsDataContext;
+    private readonly NotificationsConfiguration _notificationsConfiguration;
+    public NotificationsCleanUpFunction(
+        INotificationRepository notificationRepository, 
+        IProviderRelationshipsDataContext providerRelationshipsDataContext, 
+        IOptions<NotificationsConfiguration> notificationsConfiguration
+    )
+    {
+        _notificationRepository = notificationRepository;
+        _providerRelationshipsDataContext = providerRelationshipsDataContext;
+        _notificationsConfiguration = notificationsConfiguration.Value;
+    }
+
+    [Function(nameof(NotificationsCleanUpFunction))]
+    public async Task Run([TimerTrigger("%NotificationsCleanUpFunctionSchedule%", RunOnStartup = true)] TimerInfo timer, FunctionContext executionContext, CancellationToken cancellationToken)
+    {
+        var notifications = await _notificationRepository.GetExpiredNotifications(
+            _notificationsConfiguration.NotificationRetentionDays,
+            executionContext.CancellationToken
+        );
+
+        if(notifications.Count < 1)
+        {
+            return;
+        }
+
+        _notificationRepository.DeleteNotifications(notifications);
+
+        await _providerRelationshipsDataContext.SaveChangesAsync(cancellationToken);
+    }
+}
