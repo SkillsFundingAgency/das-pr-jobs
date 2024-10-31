485855a56e357ab04607b8b37699d733a7dff893
shaun-downey-education <shaun.downey@education.gov.uk>
2024-10-31 10:53:01 +0000

    CSP-1527: Remove comments.

--- src/SFA.DAS.PR.Jobs/MessageHandlers/Recruit/VacancyApprovedEventHandler.cs
+++ src/SFA.DAS.PR.Jobs/MessageHandlers/Recruit/VacancyApprovedEventHandler.cs
@@ -112,14 +112,6 @@ public sealed class VacancyApprovedEventHandler(
 
     private static PermissionsAudit CreatePermissionAudit(Provider provider, AccountLegalEntity accountLegalEntity)
     {
-    //    public Guid Id { get; set; }
-    //public required DateTime Eventtime { get; set; }
-    //public required string Action { get; set; }
-    //public required long Ukprn { get; set; }
-    //public required long AccountLegalEntityId { get; set; }
-    //public Guid? EmployerUserRef { get; set; }
-    //public required string Operations { get; set; }
-
         return new PermissionsAudit
         {
             Eventtime = DateTime.UtcNow,
