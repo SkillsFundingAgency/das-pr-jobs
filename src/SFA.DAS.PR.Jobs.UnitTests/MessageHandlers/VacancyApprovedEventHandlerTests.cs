e0c98633cc17e93ee5856b2392cbe5b0e5a44587
Deven Shah <devenshah1974@gmail.com>
2025-01-21 18:46:13 +0000

    CSP-1775 Refactor vacancy approved event handler to avoid call to recruit api

--- src/SFA.DAS.PR.Jobs.UnitTests/MessageHandlers/VacancyApprovedEventHandlerTests.cs
+++ src/SFA.DAS.PR.Jobs.UnitTests/MessageHandlers/VacancyApprovedEventHandlerTests.cs
@@ -1,11 +1,10 @@
-﻿using Esfa.Recruit.Vacancies.Client.Domain.Events;
+﻿using AutoFixture.NUnit3;
+using Esfa.Recruit.Vacancies.Client.Domain.Events;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 using SFA.DAS.PR.Data;
-using SFA.DAS.PR.Jobs.Infrastructure;
 using SFA.DAS.PR.Jobs.MessageHandlers.Recruit;
-using SFA.DAS.PR.Jobs.Models.Recruit;
 using SFA.DAS.PR.Jobs.Services;
 using SFA.DAS.PR.Jobs.UnitTests;
 
@@ -15,7 +14,6 @@ namespace SFA.DAS.PR.Tests.MessageHandlers.Recruit;
 public class VacancyApprovedEventHandlerTests
 {
     private Mock<ILogger<VacancyApprovedEventHandler>> _mockLogger;
-    private Mock<IRecruitApiClient> _mockRecruitApiClient;
     private ProviderRelationshipsDataContext _context;
     private Mock<IRelationshipService> _mockRelationshipService;
     private VacancyApprovedEventHandler _sut;
@@ -24,14 +22,12 @@ public class VacancyApprovedEventHandlerTests
     public void Setup()
     {
         _mockLogger = new Mock<ILogger<VacancyApprovedEventHandler>>();
-        _mockRecruitApiClient = new Mock<IRecruitApiClient>();
         _mockRelationshipService = new Mock<IRelationshipService>();
 
         _context = DbContextHelper.CreateInMemoryDbContext();
 
         _sut = new VacancyApprovedEventHandler(
             _mockLogger.Object,
-            _mockRecruitApiClient.Object,
             _context,
             _mockRelationshipService.Object);
     }
@@ -42,21 +38,10 @@ public class VacancyApprovedEventHandlerTests
         _context.Dispose();
     }
 
-    [Test]
-    public async Task Handle_ShouldProcessVacancyApprovedEventSuccessfully()
+    [Test, AutoData]
+    public async Task Handle_ShouldProcessVacancyApprovedEventSuccessfully(string accountLegalEntityPublicHashedId, long ukprn)
     {
-        var vacancyApprovedEvent = new VacancyApprovedEvent { VacancyReference = 12345 };
-        var liveVacancy = new LiveVacancyModel()
-        {
-            VacancyId = Guid.NewGuid(),
-            AccountLegalEntityPublicHashedId = "ABC123",
-            TrainingProvider = new TrainingProviderModel { Ukprn = 12345678 },
-            AccountPublicHashedId = "ACC123"
-        };
-
-        _mockRecruitApiClient
-            .Setup(x => x.GetLiveVacancy(vacancyApprovedEvent.VacancyReference, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(liveVacancy);
+        var vacancyApprovedEvent = new VacancyApprovedEvent { VacancyReference = 12345, AccountLegalEntityPublicHashedId = accountLegalEntityPublicHashedId, Ukprn = ukprn };
 
         _mockRelationshipService
             .Setup(x => x.CreateRelationship(
@@ -71,15 +56,11 @@ public class VacancyApprovedEventHandlerTests
 
         await _sut.Handle(vacancyApprovedEvent, mockContext.Object);
 
-        _mockRecruitApiClient.Verify(
-            x => x.GetLiveVacancy(vacancyApprovedEvent.VacancyReference, It.IsAny<CancellationToken>()),
-            Times.Once);
-
         _mockRelationshipService.Verify(
             x => x.CreateRelationship(
                 It.Is<RelationshipModel>(model =>
-                    model.AccountLegalEntityPublicHashedId == liveVacancy.AccountLegalEntityPublicHashedId &&
-                    model.ProviderUkprn == liveVacancy.TrainingProvider.Ukprn &&
+                    model.AccountLegalEntityPublicHashedId == vacancyApprovedEvent.AccountLegalEntityPublicHashedId &&
+                    model.ProviderUkprn == vacancyApprovedEvent.Ukprn &&
                     model.NotificationTemplateName == "LinkedAccountRecruit"),
                 It.IsAny<CancellationToken>()),
             Times.Once);
--- src/SFA.DAS.PR.Jobs/Infrastructure/Events/VacancyApprovedEvent.cs
+++ src/SFA.DAS.PR.Jobs/Infrastructure/Events/VacancyApprovedEvent.cs
@@ -1,6 +1,8 @@
 ﻿namespace Esfa.Recruit.Vacancies.Client.Domain.Events;
 public class VacancyApprovedEvent
 {
+    public required string AccountLegalEntityPublicHashedId { get; set; }
+    public long Ukprn { get; set; }
     public Guid VacancyId { get; set; }
     public long VacancyReference { get; set; }
 }
