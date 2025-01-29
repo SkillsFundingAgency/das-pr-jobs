ef2d4e581a727d87084df9f44ba43fa124ace94c
Deven Shah <devenshah1974@gmail.com>
2025-01-29 11:47:01 +0000

    CSP-1775 Updated vulnerable packages

--- src/SFA.DAS.PR.Jobs.MockServer/Program.cs
+++ src/SFA.DAS.PR.Jobs.MockServer/Program.cs
@@ -1,10 +1,8 @@
 ﻿using SFA.DAS.PR.Jobs.MockServer;
 
-var recruitServer = RecruitApiMockServer.Run();
 var commitmentsServer = CommitmentsV2ApiMockServer.Run();
 
 Console.WriteLine(("Press any key to stop the server..."));
 Console.ReadKey();
 
-recruitServer.Stop();
 commitmentsServer.Stop();
