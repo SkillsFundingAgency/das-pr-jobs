43f0e7d58b9d852c5cb5cfc18e3157a3d9916f80
Deven Shah <devenshah1974@gmail.com>
2025-02-13 13:47:01 +0000

    CSP-1464 Update how we connect to database

--- src/SFA.DAS.PR.Data/Extensions/AddPrDataContextExtension.cs
+++ src/SFA.DAS.PR.Data/Extensions/AddPrDataContextExtension.cs
@@ -1,6 +1,4 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using Azure.Core;
-using Azure.Identity;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -12,36 +10,15 @@ namespace SFA.DAS.PR.Data.Extensions;
 [ExcludeFromCodeCoverage]
 public static class AddPrDataContextExtension
 {
-    private static readonly string AzureResource = "https://database.windows.net/";
-
-    private static readonly ChainedTokenCredential AzureTokenProvider = new ChainedTokenCredential(
-        new ManagedIdentityCredential(),
-        new AzureCliCredential(),
-        new VisualStudioCodeCredential(),
-        new VisualStudioCredential()
-    );
-
     public static IServiceCollection AddPrDataContext(this IServiceCollection services, IConfiguration configuration)
     {
         var sqlConnectionString = configuration["SqlConnectionString"]!;
-        var environmentName = configuration["EnvironmentName"]!;
 
         services.AddDbContext<ProviderRelationshipsDataContext>((serviceProvider, options) =>
         {
             SqlConnection connection = null!;
 
-            if (!environmentName.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase))
-            {
-                connection = new SqlConnection
-                {
-                    ConnectionString = sqlConnectionString,
-                    AccessToken = AzureTokenProvider.GetToken(new TokenRequestContext(scopes: new string[] { AzureResource })).Token
-                };
-            }
-            else
-            {
-                connection = new SqlConnection(sqlConnectionString);
-            }
+            connection = new SqlConnection(sqlConnectionString);
 
             options.UseSqlServer(
                 connection,
@@ -49,10 +26,6 @@ public static class AddPrDataContextExtension
         });
 
         services.AddTransient<IProviderRelationshipsDataContext, ProviderRelationshipsDataContext>(provider => provider.GetService<ProviderRelationshipsDataContext>()!);
-
-        services
-            .AddHealthChecks()
-            .AddDbContextCheck<ProviderRelationshipsDataContext>();
 
         RegisterRepositories(services);
 
